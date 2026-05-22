1466082f1d8053bd5e98242ce0bbfca9e19abcc0
Orlik Louis <louis@HOME-PC.localdomain>
2026-05-23 02:13:36 +0300

    рефакторинг

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,21 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
+  QCoreApplication app(argc, argv);
 
-    auto db = QSqlDatabase::addDatabase("QPSQL");
-    db.setHostName("127.0.0.1");
-    db.setDatabaseName("cad_test_db");
-    db.setUserName("louis301");
-    db.setPassword("123");
+  auto db = QSqlDatabase::addDatabase("QPSQL");
+  db.setHostName("127.0.0.1");
+  db.setDatabaseName("cad_test_db");
+  db.setUserName("louis301");
+  db.setPassword("123");
 
-    if (!db.open()) {
-        qCritical() << "DB error:" << db.lastError().text();
-        return -1;
-    }
+  if (!db.open()) {
+    qCritical() << "DB error:" << db.lastError().text();
+    return -1;
+  }
 
-    qDebug() << "- Connected to PostgreSQL";
-    qDebug() << "- Qt version:" << QT_VERSION_STR;
+  qDebug() << "- Connected to PostgreSQL";
+  qDebug() << "- Qt version:" << QT_VERSION_STR;
 
-    return 0;
+  return 0;
 }
